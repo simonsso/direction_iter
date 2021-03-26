0be5f96fed082d9999b43ca465404653e21b3851
Fredrik Simonsson <simson@thesimson.net>
2021-03-26 14:22:52 +0900

    Initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
 use std::iter::Iterator;
 
-struct DirectionIter {}
+struct DirectionIter {
+    start_pos: (usize, usize),
+    limits: (usize, usize),
+    deltavector: Vec<(isize, isize)>,
+}
 impl DirectionIter {
-
-    /// Todo store all direction vectors 
+    /// Todo store all direction vectors
     /// store x,y start tuple
-    pub fn new( start:(usize,usize), limit:(usize,usize) ) -> Self {
-        DirectionIter {}
+    pub fn new(start: (usize, usize), limit: (usize, usize)) -> Self {
+        DirectionIter {
+            start_pos: start,
+            limits: limit,
+            deltavector: vec![(0, 1), (0, -1), (-1, 0), (1, 0)],
+        }
     }
 
     //Todo create constructor for 4 ans 8 directions
 }
 
 impl Iterator for DirectionIter {
-    type Item = (usize,usize);
+    type Item = (usize, usize);
 
-    fn next(&mut self) -> Option<(usize,usize)> {
+    fn next(&mut self) -> Option<(usize, usize)> {
+        while let Some((dx, dy)) = self.deltavector.pop() {
+            let x = self.start_pos.0 as isize + dx;
+            let y = self.start_pos.1 as isize + dy;
+            if x >= 0 && y >= 0 {
+                let x = x as usize;
+                let y = y as usize;
+                if x < self.limits.0 && y < self.limits.1 {
+                    return Some((x, y));
+                }
+            }
+        }
         None
     }
 }
@@ -24,28 +42,14 @@ impl Iterator for DirectionIter {
 mod tests {
     #[test]
     fn it_works() {
-        let mut it = super::DirectionIter::new((0,0),(10,10));
-        let ans:Vec<(usize,usize)> = it.collect();
-        assert_eq!(ans,vec![(0,1),(1,0)]);
+        let mut it = super::DirectionIter::new((0, 0), (10, 10));
+        let ans: Vec<(usize, usize)> = it.collect();
+        assert_eq!(ans, vec![ (1, 0) , (0, 1)]);
 
-        let mut it = super::DirectionIter::new((10,10),(100,100));
-        let ans:Vec<(usize,usize)> = it.collect();
-        assert_eq!(ans,vec![(10,11),(10,9),(9,10),(11,10)]);
+        let mut it = super::DirectionIter::new((10, 10), (100, 100));
+        let ans: Vec<(usize, usize)> = it.collect();
+        assert_eq!(ans, vec![(11, 10), (9, 10), (10, 9), (10, 11), ]);
     }
 }
 
-//  THIS is the code I will replace
-// if (0..matrix.len()).contains(&(x-1)) && (0..matrix[0].len()).contains(&y) {
-//     if matrix[x][y] <= matrix[x-1][y-0] {
-//         q.push_back((x,y))
-//     }
-// }
-// if (0..matrix.len()).contains(&(x+1)) && (0..matrix[0].len()).contains(&y) {
 
-// }
-// if (0..matrix.len()).contains(&x) && (0..matrix[0].len()).contains(&(y-1)) {
-
-// }
-// if (0..matrix.len()).contains(&x) && (0..matrix[0].len()).contains(&(y+1)) {
-
-// }     
